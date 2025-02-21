c9e25ea84a0ff7ed2bdab90edbb8d98b988decd2
Paul EDIDE <paul.edide@gmail.com>
2025-02-21 12:01:20 +0100

    Commit user component

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent  implements OnInit{
+
+  private titleSubject = new BehaviorSubject<string>('Users');
+  public titleAction$ = this.titleSubject.asObservable();
+
+  constructor(){}
+
+  public changeTitle(title:string) : void {
+
+    this.titleSubject.next(title);
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AppSettings } from '../../settings/app.settings';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { User } from '../../models/user/user';
+import { Observable } from 'rxjs';
+import { CustomHttpResponse } from '../../models/custom-http-response';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +13,27 @@ export class UserService {
 
 
   constructor(private http:HttpClient) { }
+
+  public getUsers() : Observable<User[] | HttpErrorResponse>{
+    return this.http.get<User[]>(`${this.host}user/list`);
+
+  }
+
+  public addUser(formData:FormData): Observable<User | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}user/add`,formData);
+
+  }
+
+  public updateUser(formData: FormData): Observable<User | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}user/update`, formData);
+
+  }
+
+  public deleteUser(userId: number): Observable<CustomHttpResponse> {
+    return this.http.delete<CustomHttpResponse>(`${this.host}delete/${userId}`);
+
+  }
+
 
   //Add user in cache
   public addUsersToLocalCache(users:User[]):void{
